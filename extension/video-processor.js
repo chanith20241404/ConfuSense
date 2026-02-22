580a450b466caf3b36bc5243f7e9c5e109bcb575
Nisanda Gunasinha <nisanda.20240164@iit.ac.lk>
2026-02-22 23:00:58 +0530

    Add optional contrast normalization to grayscale conversion

--- extension/video-processor.js
+++ extension/video-processor.js
@@ -92,11 +92,21 @@ class ConfuSenseVideoProcessor {
     return { imageData: grayscale, canvas: cropCanvas, boundingBox: { x, y, width, height } };
   }
 
-  toGrayscale(imageData) {
+  toGrayscale(imageData, normalize = false) {
     const data = imageData.data;
+    let min = 255, max = 0;
     for (let i = 0; i < data.length; i += 4) {
       const gray = 0.299 * data[i] + 0.587 * data[i + 1] + 0.114 * data[i + 2];
       data[i] = data[i + 1] = data[i + 2] = gray;
+      if (gray < min) min = gray;
+      if (gray > max) max = gray;
+    }
+    if (normalize && max > min) {
+      const range = max - min;
+      for (let i = 0; i < data.length; i += 4) {
+        const norm = ((data[i] - min) / range) * 255;
+        data[i] = data[i + 1] = data[i + 2] = norm;
+      }
     }
     return imageData;
   }
