1352d84687713658b5dce6eb9d9ed18ba44c1dc8
Nisanda Gunasinha <nisanda.20240164@iit.ac.lk>
2026-03-11 12:00:00 +0530

    Initialize Drizzle ORM client with LibSQL driver

---
+++ backend/src/db/client.ts
@@ -0,0 +1,8 @@
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
+import { config } from '../config.js';
+import * as schema from './schema.js';
+
+const client = createClient({ url: config.LIBSQL_URL });
+
+export const db = drizzle(client, { schema });
