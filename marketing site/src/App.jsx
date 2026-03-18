d2d443a4e0dc19a08007cbbab215586f24179d25
Faraz Ahamed <faraz.20240253@iit.ac.lk>
2026-03-18 13:00:00 +0530

    Create App component with page routing

---
+++ marketing site/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Pricing from './pages/Pricing';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
